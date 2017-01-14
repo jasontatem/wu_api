c4eb94f8a2dddb4a88fd7a53b72a866011f346b6
jat <jattdb@gmail.com>
2017-01-14 15:33:18 -0500

    Adding additional endpoint wrappers, bestforecast switch

--- wu_api.py
+++ wu_api.py
@@ -12,9 +12,13 @@ def yesterday_yymmdd():
 
 
 class WUApi(object):
-    def __init__(self, api_key, default_loc='CA/San_Francisco', base_url='https://api.wunderground.com/api/', language='EN'):
+    def __init__(self, api_key, default_loc='CA/San_Francisco', base_url='https://api.wunderground.com/api/', language='EN', bestforecast_enable=True):
         self.api_key = api_key
-        self.base_url = base_url + self.api_key + '/lang:{0}'.format(language)
+        if not bestforecast_enable:
+            bfc_url = '/bestfct:0'
+        else:
+            bfc_url = ''
+        self.base_url = base_url + self.api_key + '/lang:{0}'.format(language) + bfc_url
         self.default_loc = default_loc
 
     def call_wu_api(self, features, location=None):
@@ -47,4 +51,40 @@ class WUApi(object):
             results.append(self.get_history(location, query_yyyymmdd))
             query_dt = query_dt + datetime.timedelta(days=1)
         return results
+
+    def get_hourly_forecast_36h(self, location=None):
+        return self.call_wu_api('hourly', location)
+
+    def get_hourly_forecast_10d(self, location=None):
+        return self.call_wu_api('hourly10day', location)
+
+    def get_forecast_3d(self, location=None):
+        return self.call_wu_api('forecast', location)
+
+    def get_forecast_10d(self, location=None):
+        return self.call_wu_api('forecast10day', location)
+
+    def get_geolookup(self, location=None):
+        return self.call_wu_api('geolookup', location)
+
+    def get_alerts(self, location=None):
+        return self.call_wu_api('alerts', location)
+
+    def get_almanac(self, location=None):
+        return self.call_wu_api('almanac', location)
+
+    def get_astronomy(self, location=None):
+        return self.call_wu_api('astronomy', location)
+
+    def get_planner(self, location=None):
+        return self.call_wu_api('planner', location)
+
+    def get_raw_tidal(self, location=None):
+        return self.call_wu_api('rawtide', location)
+
+    def get_tidal(self, location=None):
+        return self.call_wu_api('tide', location)
+
+    def get_webcams(self, location=None):
+        return self.call_wu_api('webcams', location)
 
