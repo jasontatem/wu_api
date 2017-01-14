c5cefcbdd59d8e722ad7b87ace64f97a7ba0ff11
jat <jattdb@gmail.com>
2017-01-14 10:52:15 -0500

    Removing local paths

--- test_env.py
+++ test_env.py
@@ -1,21 +1,32 @@
 import logging
 import ConfigParser
 import argparse
-from wu_api import *
+from wu_api import WUApi
 
 
-logformat = '%(asctime)s : %(levelname)s : %(message)s'
-logging.basicConfig(format=logformat, filename='./out.log',level=logging.DEBUG)
-config_path = '/home/jat/PycharmProjects/wu_api/test_config.cfg'
+# Logging configuration.  Comment out to disable, enable
+
+log_format = '%(asctime)s : %(levelname)s : %(message)s'
+logging.basicConfig(format=log_format, filename='./out.log',level=logging.DEBUG)
 
 if __name__ == '__main__':
+
+    # Get config path from command line args
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', type=str, default=config_path, help='Config file, default {0}'.format(config_path))
+    parser.add_argument('-c', type=str, default='./test_config.cfg', help='Config file path, default ./test_config.cfg')
     args = parser.parse_args()
-    config_file = args.c
+
+    # Load config file
+
     conf_parser = ConfigParser.ConfigParser()
-    conf_parser.read(config_file)
+    conf_parser.read(args.c)
 
     api_secret = conf_parser.get('wu_api', 'api_secret')
     default_loc = conf_parser.get('wu_api', 'default_loc')
+
+    # Create WUApi object
+
+    wu = WUApi(api_secret, default_loc=default_loc)
+
 
